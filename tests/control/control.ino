fc4068b305aed1fb7b71a3a34a468cf29141f8ca
Patrik Frey <p.frey92@hotmail.com>
2017-07-04 14:28:22 +0200

    added AccelStepper, made getAccelAndGyro faster

--- tests/control/control.ino
+++ tests/control/control.ino
@@ -1,9 +1,12 @@
 #include<Wire.h>
-#include "stepper_class.h"
+#include <AccelStepper.h>
 
-// Definitions for Steppers
-Stepper2 steppers(4, 5, 6, 8, 9, 10); // int directionPin_1, int stepPin_1, int enablePin_1, int directionPin_2, int stepPin_2, int enablePin_2)
-// end definitions for steppers
+// Definitions for AccelStepper
+AccelStepper left(1, 5, 4); // pin 5 = step, pin 4 = direction, enable pins have to be set manually
+AccelStepper right(1, 9, 8); // pin 9 = step, pin 8 = direction
+int enablePin_1 = 6, enablePin_2 = 10;
+float speed_steppers=500.0;
+// end definitions for AccelStepper
 
 // Definitions for IMU
 const int MPU=0x68;  // I2C address of the MPU-6050
@@ -36,8 +39,21 @@ double distancePerStep = 0.079718;
 
 void setup(){
   // Steppers
-  steppers.setDirection_1(0);
-  steppers.setDirection_2(1);
+  left.setMaxSpeed(speed_steppers);
+  right.setMaxSpeed(speed_steppers);
+  left.setSpeed(speed_steppers);
+  right.setSpeed(speed_steppers);
+  left.setAcceleration(13000.0);
+  right.setAcceleration(13000.0);
+
+  left.move(3000);
+  right.move(-3000);
+
+  // enable motors
+  pinMode(enablePin_1,OUTPUT);
+  digitalWrite(enablePin_1,LOW);
+  pinMode(enablePin_2,OUTPUT);
+  digitalWrite(enablePin_2,LOW);
   
   // IMU
   setupMPU6050();
@@ -50,24 +66,47 @@ void setup(){
 
 void loop()
 {
-//  // 1
-//  steppers.stepMotors();
-//  if (steppers.getStepsToGo_1() == 0 && steppers.getStepsToGo_2() == 0)
-//  {
-//      steppers.setStepsToGo_1(10000);
-//      steppers.setStepsToGo_2(10000);
-//  }
-//  // end 1
+  int time_start_loop = micros();
 
-  // 2
-  //getAccelAndGyro(); //get values from registers of MPU6050 and convert Accelerations to m/s^2 and then to angles
-  //errorSteps = computeErrorSteps(AngleComplX);
-  steppers.stepMotors();
-  errorSteps=10000;
-  steppers.setStepsToGo_1(abs(errorSteps));
-  steppers.setStepsToGo_2(abs(errorSteps));
-  delayMicroseconds(2450);
+  int time_start = micros();
+  if(micros()-lastTime > 20000)
+  {
 
+    
+    getAccelAndGyro(); //get values from registers of MPU6050 and convert Accelerations to m/s^2 and then to angles
+    
+
+    errorSteps = computeErrorSteps(AngleComplX);
+
+//    Serial.print(", angleXcompl: ");
+//    Serial.println(AngleComplX);
+  }
+  int time_end = micros()-time_start;
+  Serial.print("time_getAccelAndGyro: ");
+  Serial.print(time_end);
+  time_start = micros();
+
+  if(left.distanceToGo() == 0)
+  {
+    digitalWrite(6,HIGH);
+  }
+  if(right.distanceToGo() == 0)
+  {
+    digitalWrite(10,HIGH);
+  }
+
+  time_end = micros()-time_start;
+  Serial.print(", time_check_dist: ");
+  Serial.print(time_end);
+  time_start = micros();
+
+  left.run();
+  right.run();
+
+  time_end = micros()-time_start;
+  Serial.print(", time_run: ");
+  Serial.print(time_end);
+  
 //  if(errorSteps >= 0)
 //  {
 //    steppers.setDirection_1(0);
@@ -93,44 +132,49 @@ void loop()
 //  }    
 //  counter ++;
 //  // end DEBUG
+
+  int time_end_loop = micros()-time_start_loop;
+  Serial.print(", time_loop: ");
+  Serial.println(time_end_loop);
 }
+
 
 void getAccelAndGyro()
 {
-  int16_t AcX_raw,AcY_raw,AcZ_raw,Tmp,GyX_raw,GyY_raw,GyZ_raw;
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
-  //read registers
-  AcX_raw=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY_raw=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ_raw=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX_raw=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY_raw=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ_raw=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  int16_t AcX_raw, AcZ_raw, GyX_raw;
 
-  delta_t=(micros()-lastTime)/1000000.0;
-  lastTime=micros();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // set pointer to register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 2, true);  // request a total of 2 registers (ACCEL_XOUT_H and ACCEL_XOUT_L)
+  AcX_raw = Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3F);  // set pointer to register 0x3F (ACCEL_ZOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 2, true);  // request a total of 2 registers (ACCEL_ZOUT_H and ACCEL_ZOUT_L)
+  AcZ_raw = Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);  // set pointer to register 0x43 (GYRO_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 2, true);  // request a total of 2 registers (GYRO_XOUT_H and GYRO_XOUT_L)
+  GyX_raw = Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  
+  delta_t = (micros()-lastTime)/1000000.0;
+  lastTime = micros();
 
   //convert to real Values
-  AcX=AcX_raw*0.000598755;
-  AcY=AcY_raw*0.000598755;
-  AcZ=AcZ_raw*0.000598755;
+  AcX = AcX_raw * 0.000598755;
+  AcZ = AcZ_raw * 0.000598755;
 
-  GyX=GyX_raw*0.00762939;
-  GyY=GyY_raw*0.00762939;
-  GyZ=GyZ_raw*0.00762939;
+  GyX = GyX_raw * 0.00762939;
 
   // Substract bias
-  AcX=AcX-accXBias;
-  AcY=AcY-accYBias;
-  AcZ=AcZ-accZBias;
-
-  GyX=GyX-gyroXBias;
-  GyY=GyY-gyroYBias;
-  GyZ=GyZ-gyroZBias;
+  AcX = AcX - accXBias;
+  AcZ = AcZ - accZBias;
+  
+  GyX = GyX - gyroXBias;
 
   computeAngles();
 }
@@ -139,17 +183,9 @@ void computeAngles()
 {
   //convert accelerometer measurement to angles
   AngleAccelX=atan2(AcY,AcZ) * 180/PI;
-  AngleAccelY=atan2(AcX,AcZ) * 180/PI;
-  //AccelAngleZ=atan(-AcX/AcZ) * 180/PI;
-
-  // integrate Gyro measurement
-  AngleGyroX = AngleGyroX + GyX * delta_t;
-  AngleGyroY = AngleGyroY + GyY * delta_t;
-  AngleGyroZ = AngleGyroZ + GyZ * delta_t;
 
   // complementary filter (high pass for Gyro angles, low pass for Accelerometer angles)
-  AngleComplX= 0.98*(AngleComplX + GyX * delta_t) + 0.02 *(AngleAccelX); 
-  AngleComplY= 0.98*(AngleComplY + GyY * delta_t) + 0.02 *(AngleAccelY); 
+  AngleComplX= 0.98*(AngleComplX + GyX * delta_t) + 0.02 *(AngleAccelX);  
 }
 
 int computeErrorSteps(double angle)
